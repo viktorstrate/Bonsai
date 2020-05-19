b21b84aeef328e6c3eb54d0e6115757d829f180f
viktorstrate <viktorstrate@gmail.com>
2020-05-19 23:13:51 +0200

    Initial Commit

---
+++ Bonsai/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Bonsai
+//
+//  Created by Viktor Strate Kløvedal on 19/05/2020.
+//  Copyright © 2020 viktorstrate. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
